e5922271bdb772d262ed59a7464fee88ffad9134
Peter Kingsley <chewngum@gmail.com>
2024-11-17 21:08:55 +0300

    removed excess text

--- carpark_queueing_web.py
+++ carpark_queueing_web.py
@@ -1,14 +1,9 @@
 
 #Import Functions Required
 import random
-import math
 import itertools
 import time
-import sqlite3
 import sys
-import os
-import io
-# os.system('clear')
 
 arrivalrate = int(sys.argv[1])
 servicetime = int(sys.argv[2])
@@ -19,20 +14,6 @@ def percentage(percent,list):
     for index, number in enumerate(list):
         if number >= percent:
             return index
-
-def checkmore(ar,st,sp):
-    cursor.execute('''SELECT 1 FROM Queuing WHERE ARRIVALA >= ? AND SERVICETIME >= ? AND SPACES <= ? AND q_98 = 0 LIMIT 1''', (ar,st,sp))
-    if cursor.fetchone() is not None:
-        return True
-    else:
-        return False
-
-def checkrow(ar,st,sp):
-    cursor.execute('''SELECT 1 FROM Queuing WHERE ARRIVALA = ? AND SERVICETIME = ? AND SPACES = ? LIMIT 1''', (ar,st,sp))
-    if cursor.fetchone() is not None:
-        return True
-    else:
-        return False
 
 def modelrun(arrivalrate,servicetime,spaces):
     arrival = 0
@@ -128,51 +109,6 @@ def modelrun(arrivalrate,servicetime,spaces):
     for value in percentiles:
         print(value,"th percentile is ",percentage(value,count_carsparked_q)," parked and ", percentage(value,count_carsqueued)," Queued", sep='')
 
-    dbitem = ["CP_Q", arrivalrate, servicetime, spaces, 1, percentage(10,count_carsqueued), percentage(20,count_carsqueued), percentage(30,count_carsqueued), percentage(40,count_carsqueued), percentage(50,count_carsqueued), percentage(60,count_carsqueued), percentage(70,count_carsqueued), percentage(80,count_carsqueued), percentage(90,count_carsqueued), percentage(95,count_carsqueued), percentage(98,count_carsqueued), percentage(99,count_carsqueued),]
-
-    conn = sqlite3.connect('carparkqueueing.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO Queuing (MODELTYPE, ARRIVALA, SERVICETIME, SPACES, QUEUEING, q_10, q_20, q_30, q_40, q_50, q_60, q_70, q_80, q_90, q_95, q_98, q_99) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', dbitem )
-    conn.commit()
-
-#Take User Inputs
-# print("Average arrivals per hour?")
-# while True:
-#     try:
-#         # Prompt the user to enter an integer
-#         arrivalrate = int(input("Integer, Minimum 1: "))
-#         if arrivalrate > 0:
-#             break  # Exit the loop if input is valid
-#     except ValueError:
-#         # Handle the error if input is not a valid integer
-#         print("That's not a valid number. Please try again.")
-
-# print("How many seconds per customer?")
-# while True:
-#     try:
-#         # Prompt the user to enter an integer
-#         servicetime = int(input("Integer, Minimum 1: "))
-#         if servicetime > 0:
-#             break  # Exit the loop if input is valid
-#     except ValueError:
-#         # Handle the error if input is not a valid integer
-#         print("That's not a valid number. Please try again.")
-
-# print("How many car spaces available?")
-# while True:
-#     try:
-#         # Prompt the user to enter an integer
-#         spaces = int(input("Integer, minimum 1: "))
-#         if spaces > arrivalrate * servicetime / 3600:
-#             break  # Exit the loop if input is valid
-#         if spaces < arrivalrate * servicetime / 3600:
-#             print("That number is below the saturation limit of ",int(arrivalrate * servicetime / 3600)+1," spaces. Please try again.",sep='')
-#         else:
-#             print("That number is not an integer. Please try again.")
-#     except ValueError:
-#         # Handle the error if input is not a valid integer
-#         print("That number is below ",arrivalrate * servicetime / 3600," or not an integer. Please try again.")
-
 # print("How many hours to run for?")
 while True:
     try:
@@ -187,7 +123,6 @@ while True:
 
 #print("Cycles per second for precision?")
 precision = 1 #int(input("Enter number above 0: "))
-
 
 #Intialise All Variables
 arrival = 0
@@ -206,34 +141,7 @@ queuetime = 0
 queuetest = 0
 hours = 0
 
-conn = sqlite3.connect('carparkqueueing.db')
-cursor = conn.cursor()
-cursor.execute('''
-        CREATE TABLE IF NOT EXISTS Queuing (
-            SESSION INTEGER PRIMARY KEY AUTOINCREMENT,
-            MODELTYPE varchar(100),
-            ARRIVALA int,
-            ARRIVALB int,
-            SERVICETIME int,
-            SPACES int,
-            QUEUEING int,
-            q_10 int,
-            q_20 int,
-            q_30 int,
-            q_40 int,
-            q_50 int,
-            q_60 int,
-            q_70 int,
-            q_80 int,
-            q_90 int,
-            q_95 int,
-            q_98 int,
-            q_99 int
-        )
-    ''')
-
 # os.system('clear')
 start_time = time.time()  
 
 modelrun(arrivalrate,servicetime,spaces)
-conn.close()
