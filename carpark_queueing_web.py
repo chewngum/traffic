90b99a91d90b67c56d8b384048eeb40b3f5405df
Peter Kingsley <chewngum@gmail.com>
2024-12-07 11:25:21 +0200

    Percentiles now based on sum rather than rebuilt as percentages

--- carpark_queueing_web.py
+++ carpark_queueing_web.py
@@ -10,9 +10,13 @@ spaces = int(sys.argv[3])
 precision = 1
 
 #Define Functions
-def percentage(percent,list):
-    for index, number in enumerate(list):
-        if number >= percent:
+def percentageoftime(percent,list):
+    cumulativesum = 0
+    totaltime = sum(list)
+    targetsum = totaltime * percent / 100
+    for index, value in enumerate(list):
+        cumulativesum += value
+        if cumulativesum >= targetsum:
             return index
 
 def modelrun(arrivalrate,servicetime,spaces):
@@ -20,9 +24,9 @@ def modelrun(arrivalrate,servicetime,spaces):
     #Intialise All Variables
     start_time = time.time()
     count_arrivals = 0
-    count_carsparked_q = [0] * int(arrivalrate * int(servicetime/600)+200)
-    count_carsqueued = [0] * int(arrivalrate * int(servicetime/600)+200)
-    carsparked_q = []
+    count_carsparked = [0] * int(arrivalrate * int(servicetime))
+    count_carsqueued = [0] * int(arrivalrate * int(servicetime))
+    carsparked = []
     cyclecount = int(10000)
     arrival = 0
     carsqueued = 0
@@ -36,62 +40,44 @@ def modelrun(arrivalrate,servicetime,spaces):
     
     for i in range (1,cyclecount * 3600 ):
         if i  > 3600*1000 and i%36000 ==0:
-            if queuetest == round(carsqueued/count_arrivals,5) or i == 3000*3600 or time.time()-start_time > 5:
+            if queuetest == round(carsqueued/count_arrivals,5) or i == 3000*3600:
                 hours = i / 3600
                 break
             else:
                 queuetest = round(carsqueued/count_arrivals,5)
                 
-                
-        # Count current carpark utilisation
-        count_carsparked_q[max(len(carsparked_q)-1,0)] += 1
-        count_carsqueued[queue] += 1
-        queuetime += queue
-
-        # Reduce parked cars time remaining by passing time
-        if len(carsparked_q) > 0:
-            carsparked_q = [item - 1 for item in carsparked_q]
-
-            # move finished cars out and queued cars in    
-            if carsparked_q[0] == 0:
-                del carsparked_q[0]
-                if queue > 0:
-                    carsparked_q.append(servicetime)
-                    queue -= 1
-
         # Check if new car arrived and at to carpark
         arrival = random.randint(1,3600 * precision)
         if arrival <= arrivalrate * precision:
             count_arrivals += 1
-            if len(carsparked_q) - 1 < spaces:
-                carsparked_q.append(servicetime)
-            if len(carsparked_q) - 1 == spaces:
+            if len(carsparked) - 1 < spaces:
+                carsparked.append(servicetime)
+            if len(carsparked) - 1 == spaces:
                 queue +=1
                 carsqueued +=1
-            if len(carsparked_q) - 1 > spaces:
+            if len(carsparked) - 1 > spaces:
                 print("more cars than spaces somehow")
 
+        # Count current carpark utilisation
+        count_carsparked[max(len(carsparked)-1,0)] += 1
+        count_carsqueued[queue] += 1
+        queuetime += queue
+
+        # Reduce parked cars time remaining by passing time
+        if len(carsparked) > 0:
+            carsparked = [item - 1 for item in carsparked]
+
+            # move finished cars out and queued cars in    
+            if carsparked[0] == 0:
+                del carsparked[0]
+                if queue > 0:
+                    carsparked.append(servicetime)
+                    queue -= 1
+
+        
+
     cyclecount = hours
-
-    #Convert Counts to Percentiles
-    count_carsqueued = list(itertools.accumulate(count_carsqueued))
-    count_carsparked_q = list(itertools.accumulate(count_carsparked_q))
-    if cyclecount > 0:
-        count_carsqueued = [round(100 * item / (cyclecount * 3600 * precision),2) for item in count_carsqueued]
-        count_carsparked_q = [round(100 * item / (cyclecount * 3600 * precision),2) for item in count_carsparked_q]
-
-    for i in range(0,100):
-        for index, number in enumerate(count_carsqueued):
-                if number >= i:
-                    count_carsqueued.append(index)
-                    break
-    for i in range(0,100):
-        for index, number in enumerate(count_carsparked_q):
-                if number >= i:
-                    if number <= count_carsqueued[0]:
-                        count_carsparked_q.append(index)
-                        break
-                        # Simple Data Outputs
+    
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Model completed in ", int(round(elapsed_time,0)), " seconds", sep='')
@@ -107,6 +93,6 @@ def modelrun(arrivalrate,servicetime,spaces):
     print("Perfect Arrivals  Demand = ", round(count_arrivals / cyclecount / precision * servicetime / 3600,2),"spaces")
     print("Random Arrivals Demand percentiles")
     for value in percentiles:
-        print(value,"th - ",percentage(value,count_carsparked_q)," parked and ", percentage(value,count_carsqueued)," Queued", sep='')
+        print(value,"th - ",percentageoftime(value,count_carsparked)," parked and ", percentageoftime(value,count_carsqueued)," Queued", sep='')
 
 modelrun(arrivalrate,servicetime,spaces)
