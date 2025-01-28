c2da25d2819859b46bdff0d681d38ccc93577372
Peter Kingsley <chewngum@gmail.com>
2025-01-28 22:01:19 +0200

    last commiet before sending to tom

--- carpark_blocking_web.py
+++ carpark_blocking_web.py
@@ -41,12 +41,12 @@ hours = 0
 #Generate Arrivals
 start_time = time.time()
 for i in range (1,cyclecount * 3600 * precision):
-    if i % 36000  == 0 and i  > 3600*500:
-        if blocktest == round(count_blocked/count_arrivals,5):
+    if i % 360000  == 0 and i  > 3600*1000:
+        if blocktest == round(count_blocked/count_arrivals,6):
             hours = i / 3600
             break
         else:
-            blocktest = round(count_blocked/count_arrivals,5)
+            blocktest = round(count_blocked/count_arrivals,6)
 
     # Check if new car arrived and add to carpark
     arrival = random.randint(1,3600 * precision)
